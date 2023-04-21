86271a13dc0005a67de7ab2d91eb61a9426fef20
Biobele <macintosh@Biobeles-MacBook-Air.local>
2023-04-21 03:49:45 +0100

    push

--- app/Http/Controllers/DepositsController.php
+++ app/Http/Controllers/DepositsController.php
@@ -66,9 +66,8 @@ class DepositsController extends Controller
 
         $upload_dir = "../cloud/uploads/proof";
         $filename = Carbon::now()->timestamp;
-        dd($filename.$request->file('proof')->getClientOriginalExtension());
 
-        $img = $request->file('proof')->move($upload_dir, $filename.$request->file('proof')->getClientOriginalExtension());
+        $img = $request->file('proof')->move($upload_dir, $filename.'.'.$request->file('proof')->getClientOriginalExtension());
 
         $newDeposit->proof_path = $filename;
         $newDeposit->save();
