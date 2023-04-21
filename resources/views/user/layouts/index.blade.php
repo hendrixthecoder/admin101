d299664f6b44c3d761e48e2bd3dc937185dd7343
Biobele <macintosh@Biobeles-MacBook-Air.local>
2023-04-21 15:56:24 +0100

    preloader

--- resources/views/user/layouts/index.blade.php
+++ resources/views/user/layouts/index.blade.php
@@ -44,7 +44,7 @@
 </head>
 <body>
     <div class="spinner"></div>
-    
+
     @include('user.layouts.headsection')
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
