090d9dfec84d22ab968219e1d7a386001ee1ac30
hendrixthecoder <biobeledev@gmail.com>
2023-04-24 18:42:48 +0100

    updated language switcher

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 
 class LocalizationController extends Controller
 {
     public function setLang($locale, Request $request) {
-        App::setLocale($locale);
-        $user = $request->user();
-        $user->locale = $locale;
-        $user->save();
+        if(array_key_exists($locale, Config::get('languages'))){
+            App::setLocale($locale);
+            $user = $request->user();
+            $user->locale = $locale;
+            $user->save();
+            
+        }else{
+            return back()->with('error', 'Language not found');
+        }
 
         return back()->with('successTrans', trans('auth.langChangSuc'));
     }
