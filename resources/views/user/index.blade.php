ee22cab309ecad611f8dfafae81eba610bda0480
hendrixthecoder <biobeledev@gmail.com>
2023-04-30 15:35:24 +0100

    update

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -204,157 +204,274 @@
     );
     </script>
     
+   
+    
+    <!-- HTML -->
+    
+    {{-- amCharts --}}
+    <!-- Styles -->
+<style>
+    #chartdiv {
+      width: 100%;
+      height: 600px;
+    }
+    </style>
+    
     <!-- Resources -->
     <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/map.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
     <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
     
     <!-- Chart code -->
     <script>
     am5.ready(function() {
     
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("chartdiv");
+    
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+    
     var data = [
       {
-        id: "US",
-        name: "United States",
-        value: 100
-      }, {
-        id: "GB",
-        name: "United Kingdom",
-        value: 100
-      }, {
-        id: "CN",
-        name: "China",
-        value: 100
-      }, {
-        id: "IN",
-        name: "India",
-        value: 100
-      }, {
-        id: "AU",
-        name: "Australia",
-        value: 100
-      }, {
-        id: "CA",
-        name: "Canada",
-        value: 100
-      }, {
-        id: "BR",
-        name: "Brasil",
-        value: 100
-      }, {
-        id: "ZA",
-        name: "South Africa",
-        value: 100
+        name: "Monica",
+        steps: 45688,
+        pictureSettings: {
+          src: "https://www.amcharts.com/wp-content/uploads/2019/04/monica.jpg"
+        }
+      },
+      {
+        name: "Joey",
+        steps: 35781,
+        pictureSettings: {
+          src: "https://www.amcharts.com/wp-content/uploads/2019/04/joey.jpg"
+        }
+      },
+      {
+        name: "Ross",
+        steps: 25464,
+        pictureSettings: {
+          src: "https://www.amcharts.com/wp-content/uploads/2019/04/ross.jpg"
+        }
+      },
+      {
+        name: "Phoebe",
+        steps: 18788,
+        pictureSettings: {
+          src: "https://www.amcharts.com/wp-content/uploads/2019/04/phoebe.jpg"
+        }
+      },
+      {
+        name: "Rachel",
+        steps: 15465,
+        pictureSettings: {
+          src: "https://www.amcharts.com/wp-content/uploads/2019/04/rachel.jpg"
+        }
+      },
+      {
+        name: "Chandler",
+        steps: 11561,
+        pictureSettings: {
+          src: "https://www.amcharts.com/wp-content/uploads/2019/04/chandler.jpg"
+        }
       }
     ];
     
-    var root = am5.Root.new("chartdiv");
-    root.setThemes([am5themes_Animated.new(root)]);
-    
-    var chart = root.container.children.push(am5map.MapChart.new(root, {}));
-    
-    var polygonSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-        geoJSON: am5geodata_worldLow,
-        exclude: ["AQ"]
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        panX: false,
+        panY: false,
+        wheelX: "none",
+        wheelY: "none",
+        paddingLeft: 50,
+        paddingRight: 40
       })
     );
     
-    var bubbleSeries = chart.series.push(
-      am5map.MapPointSeries.new(root, {
-        valueField: "value",
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    
+    var yRenderer = am5xy.AxisRendererY.new(root, {});
+    yRenderer.grid.template.set("visible", false);
+    
+    var yAxis = chart.yAxes.push(
+      am5xy.CategoryAxis.new(root, {
+        categoryField: "name",
+        renderer: yRenderer,
+        paddingRight:40
+      })
+    );
+    
+    var xRenderer = am5xy.AxisRendererX.new(root, {});
+    xRenderer.grid.template.set("strokeDasharray", [3]);
+    
+    var xAxis = chart.xAxes.push(
+      am5xy.ValueAxis.new(root, {
+        min: 0,
+        renderer: xRenderer
+      })
+    );
+    
+    // Add series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(
+      am5xy.ColumnSeries.new(root, {
+        name: "Income",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueXField: "steps",
+        categoryYField: "name",
+        sequencedInterpolation: true,
         calculateAggregates: true,
-        polygonIdField: "id"
+        maskBullets: false,
+        tooltip: am5.Tooltip.new(root, {
+          dy: -30,
+          pointerOrientation: "vertical",
+          labelText: "{valueX}"
+        })
       })
     );
     
-    var circleTemplate = am5.Template.new({});
-    
-    bubbleSeries.bullets.push(function(root, series, dataItem) {
-      var container = am5.Container.new(root, {});
-    
-      var circle = container.children.push(
-        am5.Circle.new(root, {
-          radius: 20,
-          fillOpacity: 0.7,
-          fill: am5.color(0xff0000),
-          cursorOverStyle: "pointer",
-          tooltipText: `{name}: [bold]{value}[/]`
-        }, circleTemplate)
-      );
-    
-      var countryLabel = container.children.push(
-        am5.Label.new(root, {
-          text: "{name}",
-          paddingLeft: 5,
-          populateText: true,
-          fontWeight: "bold",
-          fontSize: 13,
-          centerY: am5.p50
-        })
-      );
-    
-      circle.on("radius", function(radius) {
-        countryLabel.set("x", radius);
-      })
-    
-      return am5.Bullet.new(root, {
-        sprite: container,
-        dynamic: true
-      });
+    series.columns.template.setAll({
+      strokeOpacity: 0,
+      cornerRadiusBR: 10,
+      cornerRadiusTR: 10,
+      cornerRadiusBL: 10,
+      cornerRadiusTL: 10,
+      maxHeight: 50,
+      fillOpacity: 0.8
     });
     
-    bubbleSeries.bullets.push(function(root, series, dataItem) {
-      return am5.Bullet.new(root, {
-        sprite: am5.Label.new(root, {
-          text: "{value.formatNumber('#.')}",
-          fill: am5.color(0xffffff),
-          populateText: true,
-          centerX: am5.p50,
-          centerY: am5.p50,
-          textAlign: "center"
-        }),
-        dynamic: true
-      });
+    var currentlyHovered;
+    
+    series.columns.template.events.on("pointerover", function(e) {
+      handleHover(e.target.dataItem);
     });
     
+    series.columns.template.events.on("pointerout", function(e) {
+      handleOut();
+    });
     
-    
-    // minValue and maxValue must be set for the animations to work
-    bubbleSeries.set("heatRules", [
-      {
-        target: circleTemplate,
-        dataField: "value",
-        min: 10,
-        max: 50,
-        minValue: 0,
-        maxValue: 100,
-        key: "radius"
-      }
-    ]);
-    
-    bubbleSeries.data.setAll(data);
-    
-    updateData();
-    setInterval(function() {
-      updateData();
-    }, 2000)
-    
-    function updateData() {
-      for (var i = 0; i < bubbleSeries.dataItems.length; i++) {
-        bubbleSeries.data.setIndex(i, { value: Math.round(Math.random() * 100), id: data[i].id, name: data[i].name })
+    function handleHover(dataItem) {
+      if (dataItem && currentlyHovered != dataItem) {
+        handleOut();
+        currentlyHovered = dataItem;
+        var bullet = dataItem.bullets[0];
+        bullet.animate({
+          key: "locationX",
+          to: 1,
+          duration: 600,
+          easing: am5.ease.out(am5.ease.cubic)
+        });
       }
     }
     
+    function handleOut() {
+      if (currentlyHovered) {
+        var bullet = currentlyHovered.bullets[0];
+        bullet.animate({
+          key: "locationX",
+          to: 0,
+          duration: 600,
+          easing: am5.ease.out(am5.ease.cubic)
+        });
+      }
+    }
+    
+    
+    var circleTemplate = am5.Template.new({});
+    
+    series.bullets.push(function(root, series, dataItem) {
+      var bulletContainer = am5.Container.new(root, {});
+      var circle = bulletContainer.children.push(
+        am5.Circle.new(
+          root,
+          {
+            radius: 34
+          },
+          circleTemplate
+        )
+      );
+    
+      var maskCircle = bulletContainer.children.push(
+        am5.Circle.new(root, { radius: 27 })
+      );
+    
+      // only containers can be masked, so we add image to another container
+      var imageContainer = bulletContainer.children.push(
+        am5.Container.new(root, {
+          mask: maskCircle
+        })
+      );
+    
+      // not working
+      var image = imageContainer.children.push(
+        am5.Picture.new(root, {
+          templateField: "pictureSettings",
+          centerX: am5.p50,
+          centerY: am5.p50,
+          width: 60,
+          height: 60
+        })
+      );
+    
+      return am5.Bullet.new(root, {
+        locationX: 0,
+        sprite: bulletContainer
+      });
+    });
+    
+    // heatrule
+    series.set("heatRules", [
+      {
+        dataField: "valueX",
+        min: am5.color(0xe5dc36),
+        max: am5.color(0x5faa46),
+        target: series.columns.template,
+        key: "fill"
+      },
+      {
+        dataField: "valueX",
+        min: am5.color(0xe5dc36),
+        max: am5.color(0x5faa46),
+        target: circleTemplate,
+        key: "fill"
+      }
+    ]);
+    
+    series.data.setAll(data);
+    yAxis.data.setAll(data);
+    
+    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+    cursor.lineX.set("visible", false);
+    cursor.lineY.set("visible", false);
+    
+    cursor.events.on("cursormoved", function() {
+      var dataItem = series.get("tooltip").dataItem;
+      if (dataItem) {
+        handleHover(dataItem)
+      }
+      else {
+        handleOut();
+      }
+    })
+    
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear();
+    chart.appear(1000, 100);
     
     }); // end am5.ready()
     </script>
     
     <!-- HTML -->
-    
-    {{-- amCharts --}}
+
     
 
 
