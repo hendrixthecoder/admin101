f54139f07d7cf2d5db6016db446a324e45f74329
hendrixthecoder <biobeledev@gmail.com>
2023-05-03 07:21:16 +0100

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,8 @@
                         @endif
                     </div>
                 </div>
-            </div>            <div class="row">
+            </div>
+            <div class="row">
                 <div class="col-sm-6 mx-auto text-center">
                     <h1 class="mb-4"><a style="text-decoration: none;" href="{{ env('APP_URL') }}">{{ env('APP_NAME') }}</a></h1>
                 </div>
@@ -50,34 +51,34 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <label for="settingsInputEmail" class="form-label">Email address</label>
-                                        <input name="email" value="{{ old('email') }}" type="email" class="form-control" id="settingsInputEmail" aria-describedby="settingsEmailHelp" placeholder="example@neptune.com">
+                                        <input name="email" required value="{{ old('email') }}" type="email" class="form-control" id="settingsInputEmail" aria-describedby="settingsEmailHelp" placeholder="example@neptune.com">
                                         <div id="settingsEmailHelp" class="form-text">We'll never share your email with anyone else.</div>
                                     </div>
                                     <div class="col-md-6">
                                         <label for="settingsPhoneNumber" class="form-label">Phone Number</label>
-                                        <input name="p_number" type="text" value="{{ old('p_number') }}" class="form-control" id="settingsPhoneNumber" placeholder="(xxx) xxx-xxxx">
+                                        <input name="p_number" required type="text" value="{{ old('p_number') }}" class="form-control" id="settingsPhoneNumber" placeholder="(xxx) xxx-xxxx">
                                     </div>
                                 </div>
                                 <div class="row m-t-lg">
                                     <div class="col-md-6">
                                         <label for="settingsInputFirstName" class="form-label">First Name</label>
-                                        <input name="f_name" value="{{ old('f_name') }}" type="text" class="form-control" id="settingsInputFirstName" placeholder="John">
+                                        <input name="f_name" required value="{{ old('f_name') }}" type="text" class="form-control" id="settingsInputFirstName" placeholder="John">
                                     </div>
                                     <div class="col-md-6">
                                         <label for="settingsInputLastName" class="form-label">Last Name</label>
-                                        <input name="l_name" value="{{ old('l_name') }}" type="text" class="form-control" id="settingsInputLastName" placeholder="Doe">
+                                        <input name="l_name" required type="text" value="{{ old('l_name') }}" class="form-control" id="settingsInputLastName" placeholder="Doe">
                                     </div>
                                 </div>
                                 <div class="row m-t-lg">
                                     <div class="col-md-6">
                                         <label for="settingsInputUserName" class="form-label">Username</label>
                                         <div class="input-group">
-                                            <input name="username" value="{{ old('username') }}" type="text" class="form-control" id="settingsInputUserName" aria-describedby="settingsInputUserName-add" placeholder="Username">
+                                            <input name="username" required value="{{ old('username') }}" type="text" class="form-control" id="settingsInputUserName" aria-describedby="settingsInputUserName-add" placeholder="Username">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <label for="country" class="form-label">Country</label>
-                                        <select class="form-control" required name="country" id="country" tabindex="-1" style="width: 100%">
+                                        <select required class="form-control" required name="country" id="country" tabindex="-1" style="width: 100%">
                                             <option value="">Select Country</option>
                                             @foreach ($countries as $country)
                                                 <option value="{{ $country }}">{{ $country }}</option>
@@ -88,7 +89,7 @@
                                 <div class="row m-t-lg">
                                     <div class="col-md-6">
                                         <label for="signUpPassword" class="form-label">Password</label>
-                                        <input type="password" name="password" class="form-control" id="signUpPassword" aria-describedby="signUpPassword" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+                                        <input type="password" required name="password" class="form-control" id="signUpPassword" aria-describedby="signUpPassword" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
                                     </div>
                                     @if ($key ?? '')                                        
                                         <div class="col-md-6">
@@ -102,7 +103,8 @@
                                         </div>
                                     @endif
                                 </div>
-                                <a href="" class="btn btn-primary auth-submit mt-4" onclick="event.preventDefault();document.getElementById('reg-form').submit();">Sign Up</a>
+                                <input type="submit" value="Submit" class="btn btn-primary auth-submit mt-4">
+                                {{-- <a href="" class="btn btn-primary auth-submit mt-4" onclick="event.preventDefault();document.getElementById('reg-form').submit();">Sign Up</a> --}}
                             </form>
                         </div>
                     </div>
