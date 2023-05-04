c8c5760690c62cd71cf547028e2417ced669ca0a
hendrixthecoder <biobeledev@gmail.com>
2023-05-04 01:17:57 +0100

    update

--- resources/views/user/layouts/headsection.blade.php
+++ resources/views/user/layouts/headsection.blade.php
@@ -1,9 +1,9 @@
 <div class="app align-content-stretch d-flex flex-wrap">
     <div class="app-sidebar">
         <div class="logo" style="">
-            <div class="" style="border-radius:50%; overflow:hidden">
+
                 <img src="{{ env('APP_URL') }}cloud/uploads/pfp/empty.png" alt="" style="max-width:80px; border-radius:50%; max-height:80px;" >
-            </div>
+
 
             {{-- <a href="{{ route('home') }}" class="logo-icon"><span class="logo-text">Admin101</span></a> --}}
             <div class="sidebar-user-switcher user-activity-online">
