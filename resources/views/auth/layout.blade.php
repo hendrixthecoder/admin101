162cec5a1dc57c94799cc6415670cc3c72143567
hendrixthecoder <biobeledev@gmail.com>
2023-05-08 17:03:24 +0100

    updated og tags

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Responsive Admin Dashboard Template">
-    <meta name="keywords" content="admin,dashboard">
+    <meta name="description" content="Finance at it's best">
+    <meta name="keywords" content="finance,financial freedom">
     <meta name="author" content="stacks">
     <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     
